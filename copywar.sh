4f24557ab2a62e302e7d79d4a64bb9aa47e0b477
polarbear08 <polarbear08@gmail.com>
2020-03-24 11:57:19 +0000

    added comments

--- copywar.sh
+++ copywar.sh
@@ -23,39 +23,52 @@ webapps=/opt/tomcat/webapps
 
 ## war作成処理 ###########################################
 ## warファイル名を設定
+echo "warファイルの名前を設定"
 originalWarFile=${originalWarName}.war
 targetWarFile=${targetWarName}.war
-
-## Apache
-# リバースプロキシの情報設定
-echo "ProxyPass /${targetWarName} ajp://localhost:8009/${targetWarName}" >> ${proxyConfFile}
-echo "ProxyPassReverse /${targetWarName} ajp://localhost:8009/${targetWarName}" >> ${proxyConfFile}
+echo "元ファイル：${originalWarFile}"
+echo "作成ファイル：${targetWarName}"
 
 ## MySQL
+echo "STEP1/4 create schema and insert data into MySQL"
 # スキーマを作成・データ流入
-mysql -u ${dbUser} -p${dbPass}　-P ${dbPortNumber} -h ${dbHostName} -e "create database if not exists ${schemaName};" 
+mysql -u ${dbUser} -p${dbPass} -P ${dbPortNumber} -h ${dbHostName} -e "create database if not exists ${schemaName};" 
+echo "done!\n"
 
 ## warファイル
 # ワーキングディレクトリでwarファイルを展開
+echo "STEP2/4 make customized WAR"
 mkdir work
 cp ${originalWarFile} work/
 cd work
-jar xvf ${originalWarFile}
+jar xf ${originalWarFile}
 rm ${originalWarFile}
 
 # 必要な部分を書き換え
 sed -i -e "s/Apache Tomcat Examples/Kaizan Shimasita/g" index.html
 
 # warファイルを作成
-jar cvf ../${targetWarFile} .
+jar cf ../${targetWarFile} .
 cd ../
 rm -rf work
 chown tomcat:tomcat ${targetWarFile}
+echo "done!\n"
 
 ## Tomcat
+echo "STEP3/4 deploy WAR file to Tomcat"
 # デプロイ
 mv ${targetWarFile} ${webapps}
+echo "done!\n"
 
+## Apache
+echo "STEP4/4 set reverse proxy (Apache httpd)"
+# リバースプロキシの情報設定
+echo "ProxyPass /${targetWarName} ajp://localhost:8009/${targetWarName}" >> ${proxyConfFile}
+echo "ProxyPassReverse /${targetWarName} ajp://localhost:8009/${targetWarName}" >> ${proxyConfFile}
+echo "" >> ${proxyConfFile}
 
 # httpdを再起動
 systemctl restart httpd
+
+echo "done!\n"
+echo "complete!!\n\n"
