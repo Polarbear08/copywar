ca028cd934bf951910975081a64849d7225b1028
polarbear08 <polarbear08@gmail.com>
2020-03-24 11:48:31 +0000

    added comments

--- copywar.sh
+++ copywar.sh
@@ -1,36 +1,61 @@
-#!/bin/bash
+co  #!/bin/bash
 
-# 変数設定
+## 変数設定 ###########################################
+# もとのwarファイルの名前
 originalWarName=examples
-originalWarFile=${originalWarName}.war
-targetWarName=examples3
-targetWarFile=${targetWarName}.war
-proxyConfFile=/etc/httpd/conf.d/proxy.conf
-webapps=/opt/tomcat/webapps
+
+# 作成するwarファイルの名前
+targetWarName=examples4
+
+# 接続するDBの情報(MySQL)
 dbUser=root
 dbPass=P@ssw0rd
-schema=${targetWarName}
+dbHostName=localhost
+dbPortNumber=3306
+schemaName=${targetWarName}
+
+# ProxyPassを設定するファイルの名前
+proxyConfFile=/etc/httpd/conf.d/proxy.conf
+
+# tomcatのwebappsディレクトリ
+webapps=/opt/tomcat/webapps
 
 
-#echo ${targetWarName}
-#echo ${proxyConfFile}
-# ProxyPass設定
+## war作成処理 ###########################################
+## warファイル名を設定
+originalWarFile=${originalWarName}.war
+targetWarFile=${targetWarName}.war
+
+## Apache
+# リバースプロキシの情報設定
 echo "ProxyPass /${targetWarName} ajp://localhost:8009/${targetWarName}" >> ${proxyConfFile}
 echo "ProxyPassReverse /${targetWarName} ajp://localhost:8009/${targetWarName}" >> ${proxyConfFile}
 
-# DB設定
-mysql -u ${dbUser} -p${dbPass} -e "create database if not exists ${schema};" 
+## MySQL
+# スキーマを作成・データ流入
+mysql -u ${dbUser} -p${dbPass}　-P ${dbPortNumber} -h ${dbHostName} -e "create database if not exists ${schemaName};" 
 
-# war設定
+## warファイル
+# ワーキングディレクトリでwarファイルを展開
 mkdir work
 cp ${originalWarFile} work/
 cd work
 jar xvf ${originalWarFile}
 rm ${originalWarFile}
+
+# 必要な部分を書き換え
 sed -i -e "s/Apache Tomcat Examples/Kaizan Shimasita/g" index.html
+
+# warファイルを作成
 jar cvf ../${targetWarFile} .
 cd ../
+rm -rf work
 chown tomcat:tomcat ${targetWarFile}
+
+## Tomcat
+# デプロイ
 mv ${targetWarFile} ${webapps}
 
+
+# httpdを再起動
 systemctl restart httpd
